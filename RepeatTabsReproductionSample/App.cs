e48d6b9648c2ccccacc331d2f224cc530428dbe6
brminnick <brandon.r.minnick@gmail.com>
2017-12-03 21:30:12 -0500

    First Commit

---
+++ RepeatTabsReproductionSample/App.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace RepeatTabsReproductionSample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var samplePage1 = new ContentPage { Title = "1" };
+            var samplePage2 = new ContentPage { Title = "2" };
+            var samplePage3 = new ContentPage { Title = "3" };
+
+            CustomTabbedPage.Instance.Children.Add(samplePage1);
+            CustomTabbedPage.Instance.Children.Add(samplePage2);
+            CustomTabbedPage.Instance.Children.Add(samplePage3);
+
+            MainPage = CustomTabbedPage.Instance;
+        }
+    }
+
+    class CustomTabbedPage : TabbedPage
+    {
+        static CustomTabbedPage _instance;
+
+        public static CustomTabbedPage Instance => _instance ?? (_instance = new CustomTabbedPage());
+    }
+}
+
